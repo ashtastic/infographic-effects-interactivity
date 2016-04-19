cd16268fb5742d1446c8ece7b789c2b5bf005973
ashtastic <amurr20@gmail.com>
2016-04-19 13:06:08 -0400

    Animated the brain svgs and their content boxes.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,107 @@
-var $psychicBox = $('.psychic-box');
-var $preciseBox = $('.precise-box');
-var $intuitiveBox = $ ('.intuitive-box');
-var $rationalBox = $('.rational-box');
-var $soundBox = $('.sound-box');
-var $logicalBox = $('.logical-box');
-var $creativeBox = $('.creative-box');
-var $musicBox = $('.music-box');
-var $impulsiveBox = $('.impulsive-box');
-var $realisticBox = $('.realistic-box');
-var $emotionalBox = $('.emotional-box');
-var $plannerBox = $('.planner-box');
+var $hOrangeEmotional = $('.l-horizontal-orange-emotional');
+var $emotional = $('#emotional');
+var $emotionalBox = $('.emotional-box')
 
-$dino1.waypoint(function (direction) {
-  if (direction == 'down') {
-    $psychicBox.addClass('js-svg-animate');
-  } else {
-    $dino1.removeClass('js-svg-animate');
-  }
-}, { offset: '25%'});
+var $hOrangeImpulsive = $('.l-horizontal-orange-impulsive');
+var $impulsive = $('#impulsive');
+var $impulsiveBox = $('.impulsive-box')
+
+var $hOrangeCreative = $('.l-horizontal-orange-creative');
+var $creative = $('#creative');
+var $creativeBox = $('.creative-box')
+
+var $hOrangeSound = $('.l-horizontal-orange-sound');
+var $sound = $('#sound');
+var $soundBox = $('.sound-box')
+
+var $hOrangeIntuitive = $('.l-horizontal-orange-intuitive');
+var $intuitive = $('#intuitive');
+var $intuitiveBox = $('.intuitive-box')
+
+var $hOrangePsychic = $('.l-horizontal-orange-psychic');
+var $psychic = $('#psychic');
+var $psychicBox = $('.psychic-box')
+
+var $hBluePlanner = $('.l-horizontal-blue-planner');
+var $planner = $('#planner');
+var $plannerBox = $('.planner-box')
+
+var $hBlueRealistic = $('.l-horizontal-blue-realistic');
+var $realistic = $('#realistic');
+var $realisticBox = $('.realistic-box')
+
+var $hBlueCritic = $('.l-horizontal-blue-critic');
+var $critic = $('#critic');
+var $criticBox = $('.critic-box')
+
+var $hBlueLogical = $('.l-horizontal-blue-logical');
+var $logical = $('#logical');
+var $logicalBox = $('.logical-box')
+
+var $hBlueRational = $('.l-horizontal-blue-rational');
+var $rational = $('#rational');
+var $rationalBox = $('.rational-box')
+
+var $hBluePrecise = $('.l-horizontal-blue-precise');
+var $precise = $('#precise');
+var $preciseBox = $('.precise-box')
+
+$emotional.on('click', function () {
+  $hOrangeEmotional.toggleClass('grow');
+  $emotionalBox.toggleClass('show');
+});
+
+$impulsive.on('click', function () {
+  $hOrangeImpulsive.toggleClass('grow');
+  $impulsiveBox.toggleClass('show');
+});
+
+$creative.on('click', function () {
+  $hOrangeCreative.toggleClass('grow');
+  $creativeBox.toggleClass('show');
+});
+
+$sound.on('click', function () {
+  $hOrangeSound.toggleClass('grow');
+  $soundBox.toggleClass('show');
+});
+
+$intuitive.on('click', function () {
+  $hOrangeIntuitive.toggleClass('grow');
+  $intuitiveBox.toggleClass('show');
+});
+
+$psychic.on('click', function () {
+  $hOrangePsychic.toggleClass('grow');
+  $psychicBox.toggleClass('show');
+});
+
+$planner.on('click', function () {
+  $hBluePlanner.toggleClass('grow');
+  $plannerBox.toggleClass('show');
+});
+
+$realistic.on('click', function () {
+  $hBlueRealistic.toggleClass('grow');
+  $realisticBox.toggleClass('show');
+});
+
+$critic.on('click', function () {
+  $hBlueCritic.toggleClass('grow');
+  $criticBox.toggleClass('show');
+});
+
+$logical.on('click', function () {
+  $hBlueLogical.toggleClass('grow');
+  $logicalBox.toggleClass('show');
+});
+
+$rational.on('click', function () {
+  $hBlueRational.toggleClass('grow');
+  $rationalBox.toggleClass('show');
+});
+
+$precise.on('click', function () {
+  $hBluePrecise.toggleClass('grow');
+  $preciseBox.toggleClass('show');
+});
